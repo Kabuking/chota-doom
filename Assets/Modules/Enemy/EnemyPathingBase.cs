f9fb718e430283471140bc2fdbf7ae55c49c5d7d
Ashirwad Gogoi <ashirwadg@gmail.com>
2024-08-27 20:18:00 +0530

    Enemy prefabs + lerping + pathing update

--- Assets/Modules/Enemy/EnemyPathingBase.cs
+++ Assets/Modules/Enemy/EnemyPathingBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EnemyPathingBase : MonoBehaviour
 {
@@ -8,15 +9,21 @@ public class EnemyPathingBase : MonoBehaviour
     public float wanderRadius = 10f; // Radius around the camera within which the object can wander
     public float cameraAvoidRange;
     public float moveSpeed = 2f; // Speed at which the object moves
-    public SlowProjectile GunScript;
+    public SlowProjectile gunScript;
+    public float playerAvoidRange;
+    public float lookRotationSpeed;
 
     [Header("Serialized for debugging")]
     [SerializeField] float initialHeight;
     [SerializeField] private Vector3 nextLocation;
+    [SerializeField] private Transform leftWall, rightWall;
+    [SerializeField] private GameObject[] players;
 
     private void Awake()
     {
         initialHeight = transform.position.y;
+        GetBounds();
+        StartCoroutine(GetPlayers());
     }
     void Start()
     {
@@ -25,17 +32,45 @@ public class EnemyPathingBase : MonoBehaviour
 
     void Update()
     {
-        if (GunScript.targetTransform != null) { 
-            transform.LookAt(GunScript.targetTransform.position);
+        if (players.Length < 2) {
+            return;
+        }
+
+        if (gunScript.targetTransform != null) {
+            Quaternion targetRotation = Quaternion.LookRotation(gunScript.targetTransform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, lookRotationSpeed * Time.deltaTime);
+            //transform.LookAt(gunScript.targetTransform.position);
         }
 
         MoveTowardsTarget();
 
         Vector3 viewportPoint = mainCamera.WorldToViewportPoint(nextLocation);
+
         if (viewportPoint.x < 0 || viewportPoint.x > 1 || viewportPoint.y < 0 || viewportPoint.y > 1)
         {
             SetNewRandomTarget();
         }
+
+        if (Mathf.Abs(transform.position.x - leftWall.position.x) < 5 ) {
+            SetNewRandomTarget();
+        }
+
+        if (Mathf.Abs(transform.position.x - rightWall.position.x) < 5 ) {
+            SetNewRandomTarget();
+        }
+
+        /*foreach (var player in players) {
+            if (Vector3.Distance(mainCamera.transform.position, player.transform.position) > cameraAvoidRange + playerAvoidRange) {
+                
+            }
+        }*/
+
+        foreach (var player in players) {
+            if (Vector3.Distance(transform.position, player.transform.position) < playerAvoidRange) {
+                SetNewRandomTarget();
+            }
+        }
+
 
         if (Vector3.Distance(transform.position, mainCamera.transform.position) < cameraAvoidRange)
         {
@@ -77,4 +112,31 @@ public class EnemyPathingBase : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, nextLocation, moveSpeed * Time.deltaTime);
     }
+
+    void GetBounds() {
+        RaycastHit hitLeft;
+        if (Physics.Raycast(transform.position, -Vector3.right, out hitLeft, 1000)) {
+            if (hitLeft.collider.CompareTag("Bounds")) {
+                Debug.Log("Hit Bounds on the left: " + hitLeft.collider.name);
+                leftWall = hitLeft.transform;
+            }
+        }
+
+        // Raycast to the right
+        RaycastHit hitRight;
+        if (Physics.Raycast(transform.position, Vector3.right, out hitRight, 1000)) {
+            if (hitRight.collider.CompareTag("Bounds")) {
+                Debug.Log("Hit Bounds on the right: " + hitRight.collider.name);
+                rightWall = hitRight.transform;
+            }
+        }
+    }
+
+    IEnumerator GetPlayers() {
+        while (players.Length < 2) {
+            players = GameObject.FindGameObjectsWithTag("Player");
+            yield return new WaitForSeconds(1);
+        }
+    }
+
 }
